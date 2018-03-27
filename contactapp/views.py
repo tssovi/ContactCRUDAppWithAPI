540b686fb812418bb3b5f7333b0bd0307dd2dc64
Ovi <t.s.s.ovi@gmail.com>
2018-03-27 16:48:43 +0600

    Initial Release

--- contactapp/views.py
+++ contactapp/views.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from rest_framework.generics import (ListCreateAPIView, RetrieveUpdateDestroyAPIView)
 
 from contactapp.models import Contact
